4196da6b5a3e51a9b0ea071e7ea9b9ca14a1a825
IntegraMantenimiento <integra.mantenimiento@outlook.com>
2026-08-01 15:51:23 -0600

    Primera versión de la página web
    Primera versión de la página web

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const formulario = document.getElementById("formulario");
+
+// formulario.addEventListener("submit", function(e){
+
+//     e.preventDefault();
+
+//     alert("¡Gracias por contactarnos! Nos comunicaremos contigo muy pronto.");
+
+//     formulario.reset();
+
+// });
